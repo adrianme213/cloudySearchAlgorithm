3f3779b24701eee9b81818dcdc14d916d99942d1
Adrian Meza <adrianme213@gmail.com>
2018-04-30 11:26:51 -0700

    [feat] - axios GET fetches data from DB server

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # cloudySearchAlgorithm
 React setup for cloudy search algorithm for an input field.
 
-To run rest server using JSON-Server, "npm run db-server"
-To start project, run "npm run start". Navigate to http://localhost:8080 to view project.
+1) To run database server using JSON-Server, "npm run db-server".
+2) Build project using "npm run build". Note, this will continue to watch for changes.
+3) To start project, "npm run start". Navigate to http://localhost:8080 to view project.
--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import axios from 'axios';
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
+const DB_URL = `http://localhost:3000/transactions`;
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      transactions: [{}]
     }
+  }
+
+  componentDidMount() {
+    this.fetchTransactionData();
+  }
+
+  fetchTransactionData() {
+    axios.get(DB_URL)
+      .then(res => {
+        this.setState({
+          transactions: res.data
+        });
+        console.log('STATE ', this.state)
+      })
+      .catch(err => {
+        console.log('Error in GET: ', err);
+      });
   }
 
   render() {
